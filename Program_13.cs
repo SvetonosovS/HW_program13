bac112765f2889b80817e22d695c3c5139a21d92
SvetonosovS <tris23@bk.ru>
2022-10-07 15:58:18 +0300

    prog13

---
+++ Program_13.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+
+int num;
+{
+while (true)
+    {
+    Console.Write("Введите целое число: ");
+    if (int.TryParse(Console.ReadLine(), out num))
+        break;
+    Console.WriteLine($"ошибка ввода");   
+    }
+ 
+Console.Write(num +" -> ");
+
+while (num >= 999)
+    {
+    num = num / 10;
+    }
+if (num <= 99)
+    {
+      Console.WriteLine("Третьей цифры нет");   
+    }  
+if (num >=100)
+    {
+num = num % 10; 
+Console.WriteLine(num);
+    }
+}
